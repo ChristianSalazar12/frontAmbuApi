9649c96c8136bb5da1ca5bf9805c268cac784ff1
ChristianSalazar12 <cristianyamith@hotmail.com>
2025-06-02 20:44:36 -0500

    feat: taildwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#222831",
+        secondary: "#F59E0B",
+        accent: "#EF4444",
+        background: "#F3F4F6",
+        text: "#111827",
+      },
+      fontFamily: {
+        sans: ['"Inter"', "sans-serif"],
+        serif: ['"Merriweather"', "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
