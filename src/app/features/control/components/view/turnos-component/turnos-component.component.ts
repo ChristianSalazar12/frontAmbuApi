6e8279b20b083162de4e6d8563e64f2f99b36659
ChristianSalazar12 <cristianyamith@hotmail.com>
2025-06-03 07:56:28 -0500

    feat: add component new turno

--- src/app/features/control/components/view/turnos-component/turnos-component.component.ts
+++ src/app/features/control/components/view/turnos-component/turnos-component.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-turnos-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './turnos-component.component.html',
-  styleUrl: './turnos-component.component.css'
+  styleUrl: './turnos-component.component.css',
 })
 export class TurnosComponentComponent {
+  fechaActual: string = '';
+  horaActual: string = '';
+  turnoInicio = '07:00';
+  turnoFin = '19:00';
 
+  turnos = [
+    {
+      ambulancia: 'Ambulancia 1',
+      paramedico: 'Carlos Pérez',
+      auxiliar: 'Lucía Martínez',
+      conductor: 'Andrés López',
+    },
+    {
+      ambulancia: 'Ambulancia 2',
+      paramedico: 'Valentina Ruiz',
+      auxiliar: 'Julián Cárdenas',
+      conductor: 'Sofía Gómez',
+    },
+  ];
+
+  ngOnInit() {
+    const now = new Date();
+    this.fechaActual = now.toLocaleDateString();
+    this.horaActual = now.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
 }
