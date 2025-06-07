2eb9bb20802ee9644363da975091612ac3647882
ChristianSalazar12 <cristianyamith@hotmail.com>
2025-06-07 02:15:58 -0500

    feat: add changes for

--- src/app/features/user/components/layout-user/layout-user.component.ts
+++ src/app/features/user/components/layout-user/layout-user.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { ServicesAmbulanceComponent } from '../view/services-ambulance/services-ambulance.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderUserComponent } from '../header-user/header-user.component';
+import { ServicesComponentComponent } from '../../../control/components/view/services-component/services-component.component';
 
 @Component({
   selector: 'app-layout-user',
-  imports: [ServicesAmbulanceComponent, SidebarComponent, HeaderUserComponent],
+  imports: [
+    ServicesComponentComponent,
+    SidebarComponent,
+    HeaderUserComponent,
+    ServicesComponentComponent,
+  ],
   templateUrl: './layout-user.component.html',
   styleUrl: './layout-user.component.css',
 })
