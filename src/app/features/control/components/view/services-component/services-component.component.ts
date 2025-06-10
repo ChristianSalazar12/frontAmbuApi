303c73cf6e95177a165b722a5db5735f9db5197b
ChristianSalazar12 <cristianyamith@hotmail.com>
2025-06-09 20:40:35 -0500

    feat: add componet

--- src/app/features/control/components/view/services-component/services-component.component.ts
+++ src/app/features/control/components/view/services-component/services-component.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Attention } from '../../../../../models/attention.model';
+import { AttentionService } from '../../../../../services/servicesAmbu/attention.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-services-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './services-component.component.html',
-  styleUrl: './services-component.component.css'
+  styleUrl: './services-component.component.css',
 })
 export class ServicesComponentComponent {
+  attentions: Attention[] = [];
+  isLoading = true;
 
+  constructor(private attentionService: AttentionService) {}
+
+  ngOnInit(): void {
+    this.attentionService.getAttentions().subscribe({
+      next: (data) => {
+        this.attentions = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error loading attentions:', err);
+        this.isLoading = false;
+      },
+    });
+  }
 }
